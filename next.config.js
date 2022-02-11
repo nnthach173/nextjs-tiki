59f29475c63c9df0024e7322704316960d48bf67
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2022-02-12 00:18:31 +0700

    vercel update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// module.exports = {
-//   reactStrictMode: true,
-// }
-
-// const isProd = process.env.NODE_ENV === 'production';
-
-// module.exports = {
-//   assetPrefix: isProd ? '/your-github-repo-name/' : '',
-// };
-
 module.exports = {
   distDir: 'build',
 };
